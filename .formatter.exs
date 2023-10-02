a25113534b4c0e05b16ff6730e76d2f8212dda86
Lin He <he9lin@gmail.com>
2023-10-01 17:27:36 -0700

    Add formatter

--- .formatter.exs
+++ .formatter.exs
@@ -1,4 +1,6 @@
-# Used by "mix format"
 [
-  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  import_deps: [:ecto, :phoenix, :typed_struct],
+  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [let: 1, return: 1],
+  subdirectories: ["priv/*/migrations"]
 ]
